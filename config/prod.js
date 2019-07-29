7bb9894c9602522a416beb2423605cf6437b6d6c
florian lahitte <lahitte.florian@gmail.com>
2019-07-29 10:53:40 -0400

    Add secret for jwt

--- config/prod.js
+++ config/prod.js
@@ -2,15 +2,15 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    // mongoURI: process.env.MONGO_URI,
-    // cookieKey: process.env.COOKIE_KEY,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+    secret: process.env.SECRET
     // facebookClientID: process.env.FACEBOOK_CLIENT_ID,
     // facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     // instagramClientID: process.env.INSTAGRAM_CLIENT_ID,
     // instagramClientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
     // linkedinClientID: process.env.LINKEDIN_CLIENT_ID,
     // linkedinClientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-    // secret: process.env.LOCAL_SECRET,
     // stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     // stripeSecretKey: process.env.STRIPE_SECRET_KEY,
     // sendGridKey: process.env.SEND_GRID_KEY,
