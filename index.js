319a4f6921a4669dd812a705412aa84496c72440
florian lahitte <lahitte.florian@gmail.com>
2019-07-29 12:08:35 -0400

    add prod on heroku

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ require('./routes/authRoutes')(app);
 // Connect Mongo Atlas
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true});
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 // Server Setup
 const PORT = process.env.PORT || 3001;
