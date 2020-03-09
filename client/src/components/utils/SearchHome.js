657d0cdd7498e5b404bfeecc883de1957b4f5482
Florian Lahitte <lahitte.florian@gmail.com>
2020-03-09 13:19:01 -0400

    fix error

--- client/src/components/utils/SearchHome.js
+++ client/src/components/utils/SearchHome.js
@@ -1,37 +1,36 @@
-import React from "react";
-import { connect } from "react-redux";
-import { compose } from "redux";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import Autocomplete from "react-google-autocomplete";
+import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Autocomplete from 'react-google-autocomplete';
 import API from './API';
 import { withRouter } from 'react-router-dom';
-import * as actions from "../actions";
-
+import * as actions from '../actions';
 
 class SearchHome extends React.Component {
   state = {
-    city: "",
-    selectCity: ""
+    city: '',
+    selectCity: ''
   };
 
   onSubmit = event => {
     event.preventDefault();
 
     // Validate city
-    if (this.state.city === ""){
-      this.setState({ selectCity: "must select city" });
+    if (this.state.city === '') {
+      this.setState({ selectCity: 'must select city' });
     } else {
-      this.setState({ selectCity: "" });
+      this.setState({ selectCity: '' });
     }
-    
+
     const form = {
       city: this.state.city,
       vehicle: this.state.vehicle
     };
-    const city = form.city
+    const city = form.city;
 
-    if(this.state.city !== ""){  
-      API.SearchVehicle(city)
+    if (this.state.city !== '') {
+      API.SearchVehicle(city);
       this.props.history.push(`/city/${city}`);
     }
   };
@@ -39,25 +38,30 @@ class SearchHome extends React.Component {
   render() {
     return (
       <MuiThemeProvider>
-        <form className='search-city' id="search-form">
+        <form className="search-city" id="search-form">
           <div className="row">
             <div className="col m12 s12">
-                <div className='box-search-home'>
+              <div className="box-search-home">
                 <label>City</label>
                 <Autocomplete
                   onPlaceSelected={place => {
                     this.setState({ city: place.formatted_address });
                   }}
-                  types={["(regions)"]}
-                  componentRestrictions={{ country: "us" }}
+                  types={['(regions)']}
+                  componentRestrictions={{ country: 'us' }}
                 />
-                </div>
-                
-                <button style={{marginTop: '32px'}} onClick={this.onSubmit} className="waves-effect waves-light btn btn-color btn-search right">Search</button>
-                <span style={{color: 'red'}}>{this.state.selectCity}</span>
+              </div>
+
+              <button
+                style={{ marginTop: '32px' }}
+                onClick={this.onSubmit}
+                className="waves-effect waves-light btn btn-color btn-search right"
+              >
+                Search
+              </button>
+              <span style={{ color: 'red' }}>{this.state.selectCity}</span>
             </div>
           </div>
-          
         </form>
       </MuiThemeProvider>
     );
@@ -68,4 +72,7 @@ function mapStateToProps(state) {
   return { auth: state.auth.authenticated };
 }
 
-export default compose(withRouter,connect(mapStateToProps, actions))(SearchHome);
+export default compose(
+  withRouter,
+  connect(mapStateToProps, actions)
+)(SearchHome);
