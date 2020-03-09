228be2754858e2559ba72093d1a8e8ead627c0fd
Florian Lahitte <lahitte.florian@gmail.com>
2020-03-09 12:36:44 -0400

    add api key

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,8 +1,8 @@
 // prod.js - production keys here!!!
 module.exports = {
-    siteUrl: 'https://still-wildwood-65496.herokuapp.com',
-    cloudinaryClientName: 'dwtc6zep7',
-    cloudinaryClientID: '596555837329564',
-    cloudinaryClientSecret:'oCn37LxVL2KbWdFIU_H89yVcz6o',
-    googleMap: 'AIzaSyCeAipRGvfEcH30zU8l2XMdvrtycHpV55g'
+  siteUrl: 'https://still-wildwood-65496.herokuapp.com',
+  cloudinaryClientName: 'dwtc6zep7',
+  cloudinaryClientID: '596555837329564',
+  cloudinaryClientSecret: 'oCn37LxVL2KbWdFIU_H89yVcz6o',
+  googleMap: 'AIzaSyCRkBa_zStbl1Gtc0EDtGYfqO5DNECKm_8'
 };
