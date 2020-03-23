ecff4ec2fb8a862eb875962e5056455830009f7a
Florian Lahitte <lahitte.florian@gmail.com>
2020-03-23 09:17:49 -0400

    search

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,5 +1,5 @@
 // prod.js - production keys here!!!
 module.exports = {
   siteUrl: 'https://still-wildwood-65496.herokuapp.com',
-  googleMap: 'AIzaSyAkjlelC3Dss0-jafEQjlP_BBPODPt1rtc'
+  googleMap: 'AIzaSyCeAipRGvfEcH30zU8l2XMdvrtycHpV55g'
 };
