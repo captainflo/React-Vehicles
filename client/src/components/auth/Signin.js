4d2f1cadc693d8807d2e8463bded330a62001432
Florian Lahitte <lahitte.florian@gmail.com>
2021-02-09 10:46:38 -0500

    remove auth linkedin

--- client/src/components/auth/Signin.js
+++ client/src/components/auth/Signin.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { compose } from "redux";
-import { connect } from "react-redux";
-import { reduxForm, Field } from "redux-form";
-import * as actions from "../actions";
-import "./Signin.css";
+import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { reduxForm, Field } from 'redux-form';
+import * as actions from '../actions';
+import './Signin.css';
 
 class Signin extends React.Component {
   onSubmit = (formProps) => {
@@ -17,25 +17,29 @@ class Signin extends React.Component {
 
     return (
       <div>
-        <form className='user-login' onSubmit={handleSubmit(this.onSubmit)}>
-          <h4 className="center">Sign in <i className="fas fa-user-alt"></i></h4>
-            <div className='row'>
-              <div className='col m6 s12'>
-                <div className="input-field">
-                <div style={{color: 'red', marginLeft: '45px'}}>{this.props.errorMessage}</div>
-                <i className="material-icons prefix">email</i>
-                  <Field
-                    name="email"
-                    type="text"
-                    component="input"
-                    autoComplete="none"
-                    placeholder="email"
-                  />
-              </div>
-              </div>
-              <div className='col m6 s12'>
+        <form className="user-login" onSubmit={handleSubmit(this.onSubmit)}>
+          <h4 className="center">
+            Sign in <i className="fas fa-user-alt"></i>
+          </h4>
+          <div className="row">
+            <div className="col m6 s12">
               <div className="input-field">
-              <i className="material-icons prefix">lock</i>
+                <div style={{ color: 'red', marginLeft: '45px' }}>
+                  {this.props.errorMessage}
+                </div>
+                <i className="material-icons prefix">email</i>
+                <Field
+                  name="email"
+                  type="text"
+                  component="input"
+                  autoComplete="none"
+                  placeholder="email"
+                />
+              </div>
+            </div>
+            <div className="col m6 s12">
+              <div className="input-field">
+                <i className="material-icons prefix">lock</i>
                 <Field
                   name="password"
                   type="password"
@@ -43,25 +47,30 @@ class Signin extends React.Component {
                   autoComplete="none"
                   placeholder="password"
                 />
-            </div>
               </div>
             </div>
-          
-            
-            <div className='row center'>
-              <div className='col m12 s12'>
-                <button className="waves-effect waves-light btn btn-color">
-                  <i className="material-icons right">cloud</i>Sign In
-                </button>
-              </div>
+          </div>
+
+          <div className="row center">
+            <div className="col m12 s12">
+              <button className="waves-effect waves-light btn btn-color">
+                <i className="material-icons right">cloud</i>Sign In
+              </button>
             </div>
-          </form>
-          <p className='center'>- OR -</p>
-          <div className='row center'>
-            <div className='col m6 s12 box-social'>
-            <li style={{listStyle:'none', paddingBottom: '10px'}}><a href="/auth/google" className="waves-effect waves-light btn social google">
-            <i className="fab fa-google"></i>google</a></li>
-            <li style={{listStyle:'none', paddingBottom: '10px'}}><a href="/auth/linkedin" className="waves-effect waves-light btn social linkedin">
+          </div>
+        </form>
+        <p className="center">- OR -</p>
+        <div className="row center">
+          <div className="col s12 ">
+            <li style={{ listStyle: 'none', paddingBottom: '10px' }}>
+              <a
+                href="/auth/google"
+                className="waves-effect waves-light btn social google"
+              >
+                <i className="fab fa-google"></i>google
+              </a>
+            </li>
+            {/* <li style={{listStyle:'none', paddingBottom: '10px'}}><a href="/auth/linkedin" className="waves-effect waves-light btn social linkedin">
             <i className="fab fa-linkedin"></i>linkedin</a></li>
             </div>
             <div className='col m6 s12 box-social-right'>
@@ -69,26 +78,23 @@ class Signin extends React.Component {
             <i className="fab fa-facebook"></i>facebook</a></li>
           
             <li style={{listStyle:'none', paddingBottom: '10px'}}><a href="/auth/instagram" className="waves-effect waves-light btn social instagram">
-            <i className="fab fa-instagram"></i>instagram</a></li>
-            </div>
+            <i className="fab fa-instagram"></i>instagram</a></li> */}
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
 
 function mapStateToPros(state) {
   return {
     errorMessage: state.auth.errorMessage,
     auth: state.auth,
-    authReducer: state.authReducer
+    authReducer: state.authReducer,
   };
 }
 
 export default compose(
-  connect(
-    mapStateToPros,
-    actions,
-  ),
-  reduxForm({ form: "signin" })
+  connect(mapStateToPros, actions),
+  reduxForm({ form: 'signin' })
 )(Signin);
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,73 +1,83 @@
-import React from "react";
-import { reduxForm, Field } from "redux-form";
-import { compose } from "redux";
-import { connect } from "react-redux";
-import * as actions from "../actions";
+import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Signup extends React.Component {
   state = {
-    errorPassword: "",
-    validPassword: "",
-    passwordConfirm: "",
-    errorEmail: "",
+    errorPassword: '',
+    validPassword: '',
+    passwordConfirm: '',
+    errorEmail: '',
   };
-  
-  handleChange = event => {
+
+  handleChange = (event) => {
     this.setState({ passwordConfirm: event.target.value });
   };
 
   onSubmit = (formProps, dispatch) => {
-
     // Validate Email
-    const email = formProps.email
+    const email = formProps.email;
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const checkEmail = re.test(email)
+    const checkEmail = re.test(email);
     // Validate Password
-    const password = formProps.password
+    const password = formProps.password;
     const pass = /^((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})/;
-    const checkPassword = pass.test(password)
-    
-    if (checkPassword){
-      this.setState({validPassword: ""})
-    } else{
-      this.setState({validPassword: "must contains one lowercase characters, one uppercase characters, one number, least 6 characters and maximum of 20"})
-    }
+    const checkPassword = pass.test(password);
 
-    if (checkEmail){
-      this.setState({ errorEmail: ""});
+    if (checkPassword) {
+      this.setState({ validPassword: '' });
     } else {
       this.setState({
-        errorEmail: "Email is invalid"
+        validPassword:
+          'must contains one lowercase characters, one uppercase characters, one number, least 6 characters and maximum of 20',
       });
     }
-      
-    if (formProps.password === this.state.passwordConfirm) {
-        this.setState({ errorPassword: ""});
+
+    if (checkEmail) {
+      this.setState({ errorEmail: '' });
     } else {
-        this.setState({
-          errorPassword: "The two password are not the same"
-        });
+      this.setState({
+        errorEmail: 'Email is invalid',
+      });
     }
-    
-    if(checkPassword  === true && checkEmail === true && formProps.password === this.state.passwordConfirm){
+
+    if (formProps.password === this.state.passwordConfirm) {
+      this.setState({ errorPassword: '' });
+    } else {
+      this.setState({
+        errorPassword: 'The two password are not the same',
+      });
+    }
+
+    if (
+      checkPassword === true &&
+      checkEmail === true &&
+      formProps.password === this.state.passwordConfirm
+    ) {
       this.props.signup(formProps, () => {
-        this.props.history.push("/feature");
+        this.props.history.push('/feature');
       });
     }
   };
 
   render() {
     const { handleSubmit } = this.props;
-  
+
     return (
       <div>
-        <form className='user-login' onSubmit={handleSubmit(this.onSubmit)}>
+        <form className="user-login" onSubmit={handleSubmit(this.onSubmit)}>
           <h4 className="center">
             Sign Up <i className="fas fa-user-plus" />
           </h4>
           <div className="input-field">
-            <div style={{color: 'red', marginLeft: '45px'}}>{this.props.errorMessage}</div>
-            <div style={{color: 'red', marginLeft: '45px'}}>{this.state.errorEmail}</div>
+            <div style={{ color: 'red', marginLeft: '45px' }}>
+              {this.props.errorMessage}
+            </div>
+            <div style={{ color: 'red', marginLeft: '45px' }}>
+              {this.state.errorEmail}
+            </div>
             <i className="material-icons prefix">email</i>
             <Field
               name="email"
@@ -80,7 +90,9 @@ class Signup extends React.Component {
           </div>
 
           <div className="input-field">
-            <div style={{color: 'red', marginLeft: '45px'}}>{this.state.validPassword}</div>
+            <div style={{ color: 'red', marginLeft: '45px' }}>
+              {this.state.validPassword}
+            </div>
             <i className="material-icons prefix">lock</i>
             <Field
               name="password"
@@ -92,7 +104,9 @@ class Signup extends React.Component {
           </div>
 
           <div className="input-field">
-            <div style={{color: 'red', marginLeft: '45px'}}>{this.state.errorPassword}</div>
+            <div style={{ color: 'red', marginLeft: '45px' }}>
+              {this.state.errorPassword}
+            </div>
             <i className="material-icons prefix">lock</i>
             <input
               name="passwordConfirm"
@@ -103,21 +117,27 @@ class Signup extends React.Component {
               onChange={this.handleChange}
             />
           </div>
-          
-          <div className='row center'>
-              <div className='col m12 s12'>
-                <button className="waves-effect waves-light btn btn-color">
-                  <i className="material-icons right">cloud</i>Sign In
-                </button>
-              </div>
+
+          <div className="row center">
+            <div className="col m12 s12">
+              <button className="waves-effect waves-light btn btn-color">
+                <i className="material-icons right">cloud</i>Sign In
+              </button>
             </div>
-          </form>
-          <p className='center'>- OR -</p>
-          <div className='row center'>
-            <div className='col m6 s12 box-social'>
-            <li style={{listStyle:'none', paddingBottom: '10px'}}><a href="/auth/google" className="waves-effect waves-light btn social google">
-            <i className="fab fa-google"></i>google</a></li>
-            <li style={{listStyle:'none', paddingBottom: '10px'}}><a href="/auth/linkedin" className="waves-effect waves-light btn social linkedin">
+          </div>
+        </form>
+        <p className="center">- OR -</p>
+        <div className="row center">
+          <div className="cols12">
+            <li style={{ listStyle: 'none', paddingBottom: '10px' }}>
+              <a
+                href="/auth/google"
+                className="waves-effect waves-light btn social google"
+              >
+                <i className="fab fa-google"></i>google
+              </a>
+            </li>
+            {/* <li style={{listStyle:'none', paddingBottom: '10px'}}><a href="/auth/linkedin" className="waves-effect waves-light btn social linkedin">
             <i className="fab fa-linkedin"></i>linkedin</a></li>
             </div>
             <div className='col m6 s12 box-social-right'>
@@ -125,9 +145,9 @@ class Signup extends React.Component {
             <i className="fab fa-facebook"></i>facebook</a></li>
           
             <li style={{listStyle:'none', paddingBottom: '10px'}}><a href="/auth/instagram" className="waves-effect waves-light btn social instagram">
-            <i className="fab fa-instagram"></i>instagram</a></li>
-            </div>
+            <i className="fab fa-instagram"></i>instagram</a></li> */}
           </div>
+        </div>
       </div>
     );
   }
@@ -138,9 +158,6 @@ function mapStateToPros(state) {
 }
 
 export default compose(
-  connect(
-    mapStateToPros,
-    actions
-  ),
-  reduxForm({ form: "signup", })
+  connect(mapStateToPros, actions),
+  reduxForm({ form: 'signup' })
 )(Signup);
